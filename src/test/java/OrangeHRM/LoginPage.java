0df84a345110976083fd08a0965947bfd012d171
botirbek.kulzhanov <botirbek.kulzhanov@fabrique.studio>
2022-02-13 19:29:36 +0300

    Added new verifications

--- src/test/java/OrangeHRM/LoginPage.java
+++ src/test/java/OrangeHRM/LoginPage.java
@@ -3,17 +3,25 @@ package OrangeHRM;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class LoginPage {
-    private static SelenideElement username = $(By.id("txtUsername")),
-                                  password = $(By.id("txtPassword")),
-                                  loginBtn = $(By.id("btnLogin"));
+    private static SelenideElement  username = $(By.id("txtUsername")),
+                            password = $(By.id("txtPassword")),
+                            loginBtn = $(By.id("btnLogin"));
+    public static SelenideElement resetPwdLink = $(By.id("forgotPasswordLink")),
+                                  orangeHRMLogo = $(By.id("divLogo"));
 
 public static void login(String name, String pwd){
     username.setValue(name);
     password.sendKeys(pwd);
     loginBtn.click();
     }
-
+    // We can directly verify in the constructor as follows below
+//    public LoginPage (){
+//        resetPwdLink.shouldBe(visible);
+//        orangeHRMLogo.shouldBe(visible);
+//        System.out.println("hello");
+//    }
 }
--- src/test/java/OrangeHRM/LoginTest.java
+++ src/test/java/OrangeHRM/LoginTest.java
@@ -2,12 +2,16 @@ package OrangeHRM;
 
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class LoginTest {
     @Test
     public void testLogin(){
         open("https://opensource-demo.orangehrmlive.com/");
-        LoginPage.login("Admin","admin123");
+        LoginPage loginPage = new LoginPage();
+        loginPage.resetPwdLink.shouldBe(visible);
+        loginPage.orangeHRMLogo.shouldBe(visible);
+        loginPage.login("Admin","admin123");
     }
 }
